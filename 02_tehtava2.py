8c4ce572aae9123865afb3975032a20b250355dc
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-24 10:47:19 +0300

    02 2. tehtävä ympyrän säde

--- 02_tehtava2.py
+++ 02_tehtava2.py
@@ -1,7 +1,12 @@
 
+#Kirjasto pi tarkkaa arvoa varten
 import math
 
+#Kysytään käyttäjältä syöte
 radius = float(input("Syötä ympyrän säde: "))
 
+#Lasketaan ympyrän pinta-ala kaavalla
 area = math.pi * radius ** 2
+
+#Tulostetaan tulos käyttäjälle
 print("Ympyrän pinta-ala on: " + str(area))
