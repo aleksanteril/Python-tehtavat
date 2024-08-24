8e600b79318a24c6a718e8c7f1002d4b9577a7e2
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-24 11:59:05 +0300

    02 6. tehtävä numerolukon koodit

---
+++ 02_tehtava6.py
@@ -0,0 +1,25 @@
+
+#Kirjasto randint varten
+import random
+
+#Arvotaan kolminumeroinen koodi ja
+#Arvotaan nelinumeroinen koodi,
+#Jonkinlainen looppi randint olisi järkevämpi, mutta
+#ei ole vielä opetettu :(
+
+print("Kolminumeroinen koodisi on: "
+    + str(random.randint(0,9))
+    + str(random.randint(0,9))
+    + str(random.randint(0,9)))
+
+print("\nNelinumeroinen koodisi on: "
+    + str(random.randint(1,6))
+    + str(random.randint(1,6))
+    + str(random.randint(1,6))
+    + str(random.randint(1,6)))
+
+
+
+
+
+
