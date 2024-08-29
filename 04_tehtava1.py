5ecec2f31e3724d0998810e377f646d89f71b9ae
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-29 14:28:08 +0300

    04 1. tehtävä while loop, kolmella jaolliset 1-1000

---
+++ 04_tehtava1.py
@@ -0,0 +1,12 @@
+
+
+#Luodaan muuttuja joka kiertää looppia niinkauan kun
+#Luku on erisuuri kuin 1000.
+luku = 1
+while(luku != 1000):
+#Jos luku on jaollinen kolmella
+#Printataan luku tuloksena konsoliin.
+    if(luku % 3 == 0):
+        print(luku)
+    luku = luku+1
+
