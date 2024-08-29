52a5b0155581e6f4442dd9a27d38da8159ae1d0a
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-29 10:45:38 +0300

    02 2. tehtävä ympyrän pinta-ala

--- 02_tehtava2.py
+++ 02_tehtava2.py
@@ -10,3 +10,5 @@ area = math.pi * radius ** 2
 
 #Tulostetaan tulos käyttäjälle
 print("Ympyrän pinta-ala on: " + str(area))
+#print(f"Ympyrän pinta-ala on: {math.pi * pow(radius, 2)}")
+#print("Ympyrän pinta-ala on: ",area,"!")
