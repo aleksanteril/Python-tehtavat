27c0a90a85fb1260766417d34d7a36a05acbfcfc
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-29 16:27:20 +0300

    04 3. tehtävä lukujen syöttö

--- 04_tehtava3.py
+++ 04_tehtava3.py
@@ -1,1 +1,22 @@
 
+#Ohjelma aloitetaan kysymällä käyttäjältä syöte ja tämä luku
+#Asetetaan pienin ja suurin muuttujien alkuluvuksi mistä verrataan
+syotto = input("Syötä luku, jos haluat lopettaa syötä tyhjä: ")
+pienin = suurin = int(syotto)
+
+#Ohjelma kysyy käyttäjältä lukuja kunnes käyttäjä syöttää tyhjän
+while (syotto != ""):
+
+    #Verrataan käyttäjän syöttämiä lukuja
+    #Ja lisätään aina pienin ja suurin vertailun avulla muistiin
+    if int(syotto) < pienin:
+        pienin = int(syotto)
+    if int(syotto) > suurin:
+        suurin = int(syotto)
+
+    syotto = input("Syötä luku, jos haluat lopettaa syötä tyhjä: ")
+
+#Kun ohjelma loppuu, tulostetaan pienin ja suurin näytölle
+print(f"\nPienin luku: {pienin}\nSuurin luku: {suurin}")
+
+
