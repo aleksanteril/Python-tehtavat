f6f502b5fcf15fa3533acb205bd60869770a7712
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-29 14:20:19 +0300

    03 4. tehtävä karkausvuosi

---
+++ 03_tehtava4.py
@@ -0,0 +1,15 @@
+
+#Kysytään vuosiluku käyttäjältä ja asetetaan se muuttujaan
+in_vuosi = int(input("Syötä tarkastettava vuosiluku: "))
+
+#Tarkistetaan logiikalla onko vuosiluku karkausvuosi
+#Ilmoitetaan käyttäjälle onko vuosiluku karkausvuosi vai ei
+
+if in_vuosi % 100 == 0 and in_vuosi % 400 == 0:
+    print("Vuosi on karkausvuosi")
+elif in_vuosi % 4 == 0 and not in_vuosi % 100 == 0:
+    print("Vuosi on karkausvuosi")
+else:
+    print("Vuosi ei ole karkausvuosi")
+
+#Ilmoitetaan käyttäjälle onko vuosiluku karkausvuosi vai ei
