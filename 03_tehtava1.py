0f7b14dfe9653572f2bbc620adf328ef81b2141b
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-29 12:30:37 +0300

    03 1. tehtävä kuhan alamitta

---
+++ 03_tehtava1.py
@@ -0,0 +1,15 @@
+
+#Kysytään kalastajan kuhan mitta
+#Ja tehdään vakiomuuttuja laskuja varten CM
+kuha_mitta = float(input("Syötä kuhan mitta senttimetreinä: "))
+kuha_alamitta = 37
+
+
+#Jos alamittainen käskee laskemaan takaisin järveen
+#Laskee samalla monta cm puuttuu mitasta
+if kuha_mitta < kuha_alamitta:
+    print(f"\nLaske kuha takaisin järveen!\n"
+          f"Kuha on {(kuha_alamitta - kuha_mitta):.1f}cm alle sallitun mitan!")
+
+
+#Kuha on alamittainen jos sen pituus on >37cm
