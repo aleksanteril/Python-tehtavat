e0fd60f8d97090b7602edbe3b67c9ca68edb513d
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-08-29 12:41:44 +0300

    03 2. tehtävä hyttiluokka kysely

---
+++ 03_tehtava2.py
@@ -0,0 +1,17 @@
+
+#Kysytään käyttäjän hyttiluokka
+hyttiluokka = input("Hei! Syötä hyttiluokkasi: ")
+
+#Tulostetaan hyttiluokan mukaan siihen liittyvä kuvaus
+#käyttäjälle
+
+if hyttiluokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella")
+elif hyttiluokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella")
+elif hyttiluokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella")
+elif hyttiluokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella")
+else:
+    print("Virheellinen hyttiluokka")
