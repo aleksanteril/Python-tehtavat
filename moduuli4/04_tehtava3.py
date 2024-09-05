83af986a1cba39efcc6a7a6edcdc97e78b155e6e
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-09-05 10:11:33 +0300

    paranneltu

--- moduuli4/04_tehtava3.py
+++ moduuli4/04_tehtava3.py
@@ -1,8 +1,9 @@
-
+pienin = suurin = None
 #Ohjelma aloitetaan kysymällä käyttäjältä syöte ja tämä luku
 #Asetetaan pienin ja suurin muuttujien alkuluvuksi mistä verrataan
 syotto = input("Syötä luku, jos haluat lopettaa syötä tyhjä: ")
-pienin = suurin = int(syotto)
+if syotto != "":
+    pienin = suurin = int(syotto)
 
 #Ohjelma kysyy käyttäjältä lukuja kunnes käyttäjä syöttää tyhjän
 while (syotto != ""):
