c977cda21a011b41a196e8b79530e6148446369e
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-09-05 14:44:52 +0300

    muunnos aliohjelma

---
+++ moduuli6/06_tehtava3.py
@@ -0,0 +1,14 @@
+
+#Funktio jossa muutetaan US gallona ja palautetaan
+#Modernin maailman litra
+
+#Yksi gallona on 3,785 litraa
+def gal_to_lit(gallon):
+    return gallon*3.785
+
+#Kysytään käyttäjältä syöte kunnes käyttäjä syöttää tyhjän
+
+galstr = input("Syötä gallona määrä, jos haluat lopettaa syötä tyhjä: ")
+while galstr != "":
+    print(gal_to_lit(float(galstr)))
+    galstr = input("Syötä gallona määrä, jos haluat lopettaa syötä tyhjä: ")
