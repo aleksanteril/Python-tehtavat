d0a3357ac0bb39a635541e039dc551aadc145d39
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-09-05 16:20:11 +0300

    pizza ohjelma

---
+++ moduuli6/06_tehtava6.py
@@ -0,0 +1,30 @@
+
+import math
+#Funktio saa parametreinä arvot pizzan halkaisija cm
+#ja pizzan hinta, funktio laskee pizzan arvon
+# euroa / neliömetri
+
+
+#Aliohjelma arvojen kysymistä varten
+def pizzatiedot(num, arvo):
+    return float(input(f"Syötä pizzan {num} {arvo}: "))
+
+    #Lasketaan pizzan pinta-ala ja muutetaan returnissa vielä neliömetreiksi
+def pizzavalue(halkaisija, hinta):
+    area = math.pi * ((halkaisija/2)**2)
+    value = hinta / (area/10000)
+    return value
+
+
+#Pääohjelmassa kysytään kahden pizzan halkaisijat ja hinnat
+#ja ilmoitetaan kummassa on parempi arvo suhteessa pinta-alaan
+pizzavaluelist = []
+for i in range (1,3):
+    pizzavaluelist.append(
+        pizzavalue(pizzatiedot(i, "halkaisija cm"), pizzatiedot(i, "hinta euroa"))
+    )
+
+if pizzavaluelist[0] < pizzavaluelist[1]:
+    print(f"Pizza 1 on parempi hintasuhde! {pizzavaluelist[0]:.2f} e/neliö")
+else:
+    print(f"Pizza 2 on parempi hintasuhde! {pizzavaluelist[1]:.2f} e/neliö")
