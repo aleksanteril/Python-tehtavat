29cab787d019b458b415fb0611a864cb4fd87583
aleksanteril <aleksanteri.laitinen@metropolia.fi>
2024-09-05 14:51:39 +0300

    listan summaus

--- moduuli6/06_tehtava4.py
+++ moduuli6/06_tehtava4.py
@@ -5,6 +5,6 @@ def lista_summaus(numlista):
 
 
 #Pääohjelma jossa kutsutaan aliohjelmaa ja tulostetaan tulos
-intlista = [1, 6 , 7 , 24, 13]
+intlista = [1, 6, 7, 24, 13]
 intlista_tulos = lista_summaus(intlista)
 print (intlista_tulos)
