9c69b1be87c9432dc380d682039a91d464ce021a
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-06 16:12:27 +0300

    parannus

--- 07_tehtava3.py
+++ 07_tehtava3.py
@@ -1,31 +1,28 @@
 
 #Luodaan sanakirja lentoasemien nimistä ja ICAO koodista
-lentoasemat = {}
+lentoasemat = {"EFFI" : "Helsinki-Vantaan lentokenttä"}
 #Funktio sanakirjaan lisäämiseen
-def syotauusi(asema, icao):
-    lentoasemat[icao] = asema
-    return
-
-
-#Funktio sanakirjasta tiedon hakemiseen
-def haetiedot(icao):
-    asema = lentoasemat[icao]
-    return asema
+def syotauusi():
+    asema = input("Syötä lentoaseman nimi: "),
+    icao = input("Syötä lentoaseman ICAO-koodi: ")
+    return asema, icao
 
 while True:
-    print("Valitse haluatko syöttää uuden, hakea tietoa tai lopettaa")
+    print("\nValitse haluatko syöttää uuden, hakea tietoa tai lopettaa")
     print("Komennot: syötä, hae, lopeta")
-    komento = input("komento: ")
+    komento = input("Komento: ")
 
     if komento == "syötä":
-        syotauusi(
-            input("Syötä lentoaseman nimi: "),
-            input("Syötä lentoaseman ICAO-koodi: "))
+        nimi, icao = syotauusi()
+        lentoasemat[icao] = nimi
     elif komento == "hae":
-        nimi = haetiedot(
-            input("Syötä ICAO-koodi: "))
-        print(f"Lentoaseman nimi on {nimi}")
+        icaoinp = input("Syötä haettava ICAO-koodi: ")
+        if icaoinp in lentoasemat:
+            print(f"{icaoinp} tunnuksen lentoasema on: {lentoasemat[icaoinp]}")
+        else:
+            print("Ei vastaavuuksia")
     elif komento == "lopeta":
+        print("\nNäkemiin!")
         break
     else:
         print("\nTuntematon syöte")
