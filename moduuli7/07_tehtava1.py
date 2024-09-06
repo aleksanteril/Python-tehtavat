06b94f98bfda3b3d2921256d6e66f5fb326025b6
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-06 16:50:38 +0300

    korjattu vuodenajat

--- moduuli7/07_tehtava1.py
+++ moduuli7/07_tehtava1.py
@@ -1,6 +1,6 @@
 
 #Monikossa säilytetään vuodenajat
-vuodenaika = ("Kevät", "Kesä", "Syksy", "Talvi")
+vuodenaika = ("Talvi", "Kevät", "Kesä", "Syksy")
 
 #Kysytään käyttäjältä vuodenaika
 kknumero = int(input("Hei! Syötä kuukauden numero 1-12: "))
@@ -9,12 +9,12 @@ kknumero = int(input("Hei! Syötä kuukauden numero 1-12: "))
 #Verrataan kknumeroa ja mikä vuodenaika se on
 if kknumero != 0:
     if kknumero <= 3:
-        print(vuodenaika[3])
+        print(vuodenaika[0])
     elif kknumero <= 6:
         print(vuodenaika[1])
     elif kknumero <= 9:
         print(vuodenaika[2])
     elif kknumero <= 12:
-        print(vuodenaika[1])
+        print(vuodenaika[3])
 else:
     print("Tuntematon")
