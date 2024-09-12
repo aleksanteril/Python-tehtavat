1822a0cf503da13bb469b7e7c904c115ebf859cf
Aleksanteri Laitinen <aleksanteri.laitinen@metropolia.fi>
2024-09-12 10:28:08 +0300

    paranneltu rakennetta ja hajoiteltu ohjelma aliohjelmiin

--- moduuli7/07_tehtava3.py
+++ moduuli7/07_tehtava3.py
@@ -1,33 +1,41 @@
 
-#Luodaan sanakirja lentoasemien nimistä ja ICAO koodista
-lentoasemat = {"EFFI" : "Helsinki-Vantaan lentokenttä"}
 
-
-#Aliohjelmassa kysytään syötettävän aseman nimi ja ICAO tunnus ja palautetaan pääohjelmaan monikkona
-def syotauusi():
-    asema, icao = input("Syötä lentoaseman nimi: "), input("Syötä lentoaseman ICAO-koodi: ")
-    return asema, icao
-
-#Pääohjelmassa loop kunnes käyttäjä lopettaa komennolla
-while True:
+#Aliohjelma valintaa varten
+def valinta():
     print("\nValitse haluatko syöttää uuden, hakea tietoa tai lopettaa")
     print("Komennot: syötä, hae, lopeta")
     komento = input("Komento: ")
+    return komento
 
+#Aliohjelma sanakirjaan syöttämistä varten
+def syotauusi(lentoasemat):
+    asema = input("Syötä lentoaseman nimi: ")
+    icao = input("Syötä lentoaseman ICAO-koodi: ")
+    lentoasemat[icao] = asema
+    return lentoasemat
+
+#Aliohjelma hakua varten
+def haku(lentoasemat):
+    icao = input("Syötä haettava ICAO-koodi: ")
+    if icao in lentoasemat:
+        print(f"{icao} tunnuksen lentoasema on: {lentoasemat[icao]}")
+    else:
+        print("Ei vastaavuuksia")
+    return
+
+
+#Luodaan sanakirja lentoasemien nimistä ja ICAO koodista
+komento = None
+lentoasemat = {"EFHK" : "Helsinki-Vantaan lentokenttä"}
+
+#Pääohjelma jossa pyöritään kunnes lopeta komento
+while komento != "lopeta":
+    komento = valinta()
     if komento == "syötä":         #syöttö ja sanakirjaan lisäys hoidetaan aliohjelman avulla
-        nimi, icao = syotauusi()   #puretaan monikko ja asetetaan sanakirjaan
-        lentoasemat[icao] = nimi
+        lentoasemat = syotauusi(lentoasemat)
     elif komento == "hae":         #Haussa tarkistetaan löytyykö vastaavuuksia ja printataan aseman nimi
-        icaoQ = input("Syötä haettava ICAO-koodi: ")
-        if icaoQ in lentoasemat:
-            print(f"{icaoQ} tunnuksen lentoasema on: {lentoasemat[icaoQ]}")
-        else:
-            print("Ei vastaavuuksia")
-    elif komento == "lopeta":      #Ohjelman lopetus
-        print("\nNäkemiin!")
-        break
-    else:                           #Jos komento tuntematon  tulostetaan käyttäjälle ilmoitus
+        haku(lentoasemat)
+    elif komento != "lopeta":                           #Jos komento tuntematon  tulostetaan käyttäjälle ilmoitus
         print("\nTuntematon syöte")
 
-
-
+print("Näkemiin!")
